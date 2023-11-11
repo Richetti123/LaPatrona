63bfa7db9726a0c18c7fce9eb4ce1dee7c95d703
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-11-10 22:45:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -197,7 +197,7 @@ const connectionOptions = {
     creds: state.creds,
     keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})),
   },
-  browser: ['MichiBot', 'Safari', '1.0.0'],
+  browser: ['ᴹᴸ᭄ La Patrona', 'Safari', '1.0.0'],
   version,
   defaultQueryTimeoutMs: undefined,
 };
